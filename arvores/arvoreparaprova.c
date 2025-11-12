e33eb295daabda8cb5ae6491c719a178f0fd8cf3
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2025-11-12 15:26:44 -0300

    Implement code changes to enhance functionality and improve performance

--- arvores/arvoreparaprova.c
+++ arvores/arvoreparaprova.c
@@ -18,3 +18,49 @@ noArvore *criar_No(int valor){
     novo_No -> valor = valor;
     return novo_No;
 }
+
+noArvore *insere(noArvore *raiz, noArvore *n){
+
+    if (raiz == NULL){
+        return n;
+    }
+    
+    if(raiz -> valor > n -> valor){
+        raiz -> esquerda = insere(raiz -> esquerda, n);
+    }else{
+        raiz -> direita = insere(raiz -> direita, n);
+            return raiz;
+    }
+}
+
+noArvore *busca(noArvore *raiz, int k){
+
+    if (raiz || raiz -> valor == k){
+        return raiz;
+    }
+
+    if (raiz -> valor > k){
+        return busca(raiz -> esquerda, k);
+    }else{
+        return busca(raiz -> direita, k);
+    }
+    
+
+}
+
+
+    
+
+
+
+int main(){
+
+    noArvore *raiz = NULL;
+    noArvore *no_inserir = criar_No(10);
+    raiz = insere(raiz, no_inserir);
+
+    printf("%d\n", raiz -> valor);
+
+
+    return 0;
+}
