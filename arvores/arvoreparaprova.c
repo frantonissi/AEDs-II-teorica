8c7b69efed282065034ff7beeaf594116ece2cbd
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2025-11-15 20:36:08 -0300

    Refactor code structure for improved readability and maintainability

--- arvores/arvoreparaprova.c
+++ arvores/arvoreparaprova.c
@@ -1,62 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 typedef struct{
-
     int valor;
-    struct noArvore *esquerda;
-    struct noArvore *direita
+    struct noArvore *right;
+    struct noArvore *left;
+} noArvore;
 
-}noArvore;
 
-noArvore *criar_No(int valor){
+noArvore *create_Node(int valor){
+
     noArvore *novo_No = (noArvore*) malloc(sizeof(noArvore));
-    novo_No -> direita = NULL;
-    novo_No -> esquerda = NULL;
     novo_No -> valor = valor;
+    novo_No -> right = NULL;
+    novo_No -> left = NULL;
     return novo_No;
 }
 
-noArvore *insere(noArvore *raiz, noArvore *n){
+
+void pre_Order(noArvore *raiz){
+    if (raiz == NULL){
+        return;
+    }
+        printf("%d", raiz -> valor);
+        pre_Order(raiz -> left);
+        pre_Order(raiz -> right);
+    
+    
+}
+
+void in_Order(noArvore *raiz){
+
+    if (raiz == NULL){
+        return;
+    }
+        in_Order(raiz -> left);
+        printf("%d", raiz -> valor);
+        in_Order(raiz -> right);
+}
+
+void pos_Order(noArvore *raiz){
+
+    if (raiz == NULL){
+        return;
+    }
+
+        pos_Order(raiz -> left);
+        pos_Order(raiz -> right);
+        printf("%d", raiz -> valor);
+
+}
+
+noArvore *insert_Node(noArvore *raiz, noArvore *n){
 
     if (raiz == NULL){
         return n;
-    }
-    
-    if(raiz -> valor > n -> valor){
-        raiz -> esquerda = insere(raiz -> esquerda, n);
+    }else if (raiz -> valor > n -> valor){
+        raiz -> left = insert_Node(raiz -> left, n);
     }else{
-        raiz -> direita = insere(raiz -> direita, n);
+        raiz -> right = insert_Node(raiz -> right, n);
             return raiz;
     }
 }
 
-noArvore *busca(noArvore *raiz, int k){
+noArvore *search(noArvore *raiz, int k){
 
-    if (raiz || raiz -> valor == k){
+    if (raiz == NULL){
+        return NULL;
+    }
+    if (raiz -> valor == k){
         return raiz;
     }
-
     if (raiz -> valor > k){
-        return busca(raiz -> esquerda, k);
+        return search(raiz -> left, k);
     }else{
-        return busca(raiz -> direita, k);
+        return search(raiz -> right, k);
     }
-    
 
 }
 
+noArvore *search_Father(noArvore *raiz, noArvore *n){
+
+    if(raiz == n){
+        return NULL;
+    }
+    if ((raiz->left != NULL && raiz->left == n) ||
+        (raiz->right != NULL && raiz->right == n))
+        return raiz;
+    if (raiz -> valor > n -> valor){
+        return search_Father(raiz -> left, n);
+    }else {
+        return search_Father(raiz -> right, n);
+    }
+
+}
 
 int main(){
 
     noArvore *raiz = NULL;
-    noArvore *no_inserir = criar_No(10);
-    raiz = insere(raiz, no_inserir);
+    noArvore *no_inserir = create_Node(10);
+    raiz = insert_Node(raiz, no_inserir);
 
-    printf("%d\n", raiz -> valor);
+    no_inserir = create_Node(5);
+    raiz = insert_Node(raiz, no_inserir);
 
+    no_inserir = create_Node(15);
+    raiz = insert_Node(raiz, no_inserir);
 
+    no_inserir = create_Node(7);
+    raiz = insert_Node(raiz, no_inserir);
     return 0;
 }
