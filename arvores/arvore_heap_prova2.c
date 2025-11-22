26feee890ddf9803c6745172f86bcc8aecbf79db
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2025-11-22 16:44:54 -0300

    Implement extraction functions for Max and Min heaps with improved logic and error handling

--- arvores/arvore_heap_prova2.c
+++ arvores/arvore_heap_prova2.c
@@ -74,6 +74,63 @@ int extracao_Max(heap *raiz){
         return -1;
     }
 
-    
+    if (raiz -> tamanho_heap == 1){
+        return raiz -> item[0];
+        raiz -> tamanho_heap--;
+    }
+
+    int raiz_removida = raiz -> item[0];
+    raiz -> item[0] = raiz -> item[raiz -> tamanho_heap - 1];
+    raiz -> tamanho_heap --;
+
+    int i = 0;
+    //a arvore é prenchida em ordem, logo, só tera filho da direita, se primeior tiver filho da esquerda
+    while(i != 0 && filho_esquerda(i) < raiz -> tamanho_heap){
+        int maior = filho_esquerda;
+        if (raiz -> item[filho_direita(i)] > raiz -> item[filho_esquerda(i)]){
+            maior = filho_direita(i);
+        }
+
+        if( raiz -> item[i] < raiz -> item[maior]){
+
+            troca(&raiz -> item[i], &raiz -> item[maior]);
+            i = maior;
+        }else{
+            break;
+        }
+    }
+    return raiz_removida;
+
+}
+
+int extracao_Min(heap *raiz){
+
+    if (raiz == NULL){
+        return -1;
+    }
+
+    if (raiz -> tamanho_heap == 0){
+        return raiz;
+        raiz -> tamanho_heap--;
+    }
+
+    int raiz_removida = raiz -> item[0];
+    raiz -> item[0] = raiz -> item[raiz -> tamanho_heap - 1];
+    raiz -> tamanho_heap--;
+
+    int i = 0;
+    while(filho_esquerda(i) < raiz -> tamanho_heap){
+        int menor = filho_esquerda;
+        if (raiz -> item[filho_direita(i)] < raiz -> item[filho_esquerda(i)]){
+            menor = filho_direita;
+        }
+        if(raiz -> item[menor] < raiz -> item[i]){
+            troca(&raiz -> item[menor], &raiz -> item[i]);
+            i = menor;
+        }else{
+            break;
+        }
+    }
+    return raiz_removida;
 
 }
